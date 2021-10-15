1e613131d174d4b87dae6ba84d740742cc122842
Zakaria <zakaria.darrah@gmail.com>
2021-10-15 11:02:51 +0200

    maj appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { PublicationComponent } from './publication/publication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
